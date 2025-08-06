cd6b1f26132fec18f096392a1f829f55248b25f7
StikyPiston <156162907+StikyPiston@users.noreply.github.com>
2025-08-06 10:07:34 +0200

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,8 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 install -y tmux git micro kitty hyprland hyprpaper hyprcursor hyprutils hyprgraphics xdg-desktop-portal-hyprland
+dnf5 install gdm3
 
 # Use a COPR Example:
 #
@@ -22,3 +23,4 @@ dnf5 install -y tmux
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
+systemctl enable gdm
